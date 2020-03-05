b4fcf624f0a4723886617c2189ba394added12e4
Taku Nakai <tnakaicode@gmail.com>
2020-03-06 05:03:14 +0900

    format glacier

--- glacier/glacier.py
+++ glacier/glacier.py
@@ -2,7 +2,7 @@ from dolfin import *
 #
 #  Discussion:
 #
-#    This FENICS input file sets up and solves the Stokes equations 
+#    This FENICS input file sets up and solves the Stokes equations
 #    for a model of a glacier.
 #
 #    The glacier is considered to lie within a rectangle tilted at
@@ -46,38 +46,45 @@ mu = 1.0E14
 #
 #  The body force vector G = (Gx,Gy).
 #
-G = Constant ( ( sin ( alpha ) * g * rho, cos ( alpha ) * g * rho ) )
+G = Constant((sin(alpha) * g * rho, cos(alpha) * g * rho))
 #
 #  Define the mesh, using 3 cells in the X and Y direction.
 #
-mesh = RectangleMesh ( 0.0, 0.0, Le, He, 3, 3 )
+mesh = RectangleMesh(0.0, 0.0, Le, He, 3, 3)
 #
 #  Specify the boundary conditions.
 #
-def LowerBoundary ( x, on_boundary ):
-  return x[1] < DOLFIN_EPS and on_boundary
 
-class PeriodicBoundary_x ( SubDomain ):
- def inside ( self, x, on_boundary ):
-    return x[0] == 0 and on_boundary
- def map ( self, x, y ):
-    y[0] = x[0] - Le
-    y[1] = x[1]
-pbc_x = PeriodicBoundary_x ( )
 
-SlipRate = Expression ( ( "(3.0 + 1.7 * sin ( 2.0 * pi / %s * x[0] ) ) / 31557686.4" % Le, "0.0" ) )
+def LowerBoundary(x, on_boundary):
+    return x[1] < DOLFIN_EPS and on_boundary
+
+
+class PeriodicBoundary_x (SubDomain):
+    def inside(self, x, on_boundary):
+        return x[0] == 0 and on_boundary
+
+    def map(self, x, y):
+        y[0] = x[0] - Le
+        y[1] = x[1]
+
+
+pbc_x = PeriodicBoundary_x()
+
+SlipRate = Expression(
+    ("(3.0 + 1.7 * sin ( 2.0 * pi / %s * x[0] ) ) / 31557686.4" % Le, "0.0"))
 #
 #  Define the function spaces:
 #    Velocity: piecewise quadratic vector.
 #    Pressure: piecewise linear scalar.
 #
-V = VectorFunctionSpace ( mesh, "CG", 2, constrained_domain = pbc_x )
-Q = FunctionSpace ( mesh, "CG", 1, constrained_domain = pbc_x )
+V = VectorFunctionSpace(mesh, "CG", 2, constrained_domain=pbc_x)
+Q = FunctionSpace(mesh, "CG", 1, constrained_domain=pbc_x)
 W = V * Q
 #
 #  Define the Dirichlet condition at the base of the glacier.
 #
-bcD = DirichletBC ( W.sub ( 0 ), SlipRate, LowerBoundary )
+bcD = DirichletBC(W.sub(0), SlipRate, LowerBoundary)
 #
 #  Define the periodic condition on the sides.
 #
@@ -86,21 +93,21 @@ bcD = DirichletBC ( W.sub ( 0 ), SlipRate, LowerBoundary )
 #
 #  Define the variational problem: a(u,v) = L(v).
 #
-( v_i, q_i ) = TestFunctions ( W )
-( u_i, p_i ) = TrialFunctions ( W )
-a = ( 0.5 * mu * inner ( grad ( v_i ) + grad ( v_i ).T, grad ( u_i ) + grad ( u_i ).T ) \
-  - div ( v_i )* pi + q_i * div ( u_i ) ) * dx
-L = inner ( v_i, G ) * dx
+(v_i, q_i) = TestFunctions(W)
+(u_i, p_i) = TrialFunctions(W)
+a = (0.5 * mu * inner(grad(v_i) + grad(v_i).T, grad(u_i) + grad(u_i).T)
+     - div(v_i) * pi + q_i * div(u_i)) * dx
+L = inner(v_i, G) * dx
 #
 #  Matrix assembly.
 #
-U = Function ( W )
+U = Function(W)
 #
 #  Solution.
 #
-solve ( a == L, U, bcD )
+solve(a == L, U, bcD)
 #
 #  Split the mixed solution to recover velocity and pressure.
 #
 #( u, p ) = U.split ( )
-#plot(u)
+# plot(u)
--- glacier/glacier_old.py
+++ glacier/glacier_old.py
@@ -2,7 +2,7 @@ from dolfin import *
 #
 #  Discussion:
 #
-#    This FENICS input file sets up and solves the Stokes equations 
+#    This FENICS input file sets up and solves the Stokes equations
 #    for a model of a glacier.
 #
 #  Modified:
@@ -43,55 +43,65 @@ mu = 1.0E14
 #
 #  Body force.
 #
-G = Constant ( ( sin ( alpha ) * g * rho, cos ( alpha ) * g * rho ) )
+G = Constant((sin(alpha) * g * rho, cos(alpha) * g * rho))
 #
 #  Define the mesh.
 #
-mesh = Rectangle ( 0.0, 0.0, Le, He, 3, 3 )
+mesh = Rectangle(0.0, 0.0, Le, He, 3, 3)
 #
 #  Define the function spaces:
 #    Velocity: piecewise quadratic vector.
 #    Pressure: piecewise linear scalar.
 #
-V = VectorFunctionSpace ( mesh, "CG", 2 )
-Q = FunctionSpace ( mesh, "CG", 1 )
+V = VectorFunctionSpace(mesh, "CG", 2)
+Q = FunctionSpace(mesh, "CG", 1)
 W = V * Q
 #
 #  Define the Dirichlet condition at the base of the glacier.
 #
-def LowerBoundary ( x, on boundary ):
-  return x[1] < DOLFIN EPS and on boundary
 
-SlipRate = Expression ( ( "(3.0 + 1.7 * sin ( 2.0 * pi / %s * x[0] ) ) / 31557686.4" % Le, "0.0" ) )
-bcD = DirichletBC ( W.sub ( 0 ), SlipRate, LowerBoundary )
+
+def LowerBoundary(x, on boundary):
+    return x[1] < DOLFIN EPS and on boundary
+
+
+SlipRate = Expression(
+    ("(3.0 + 1.7 * sin ( 2.0 * pi / %s * x[0] ) ) / 31557686.4" % Le, "0.0"))
+bcD = DirichletBC(W.sub(0), SlipRate, LowerBoundary)
 #
 #  Define the periodic condition on the sides.
 #
-class PeriodicBoundary x ( SubDomain ):
-  def inside ( self, x, on boundary ):
-    return x[0] == 0 and on boundary
- def map ( self, x, y ):
-    y[0] = x[0] - Le
-    y[1] = x [1]
-pbc_x = PeriodicBoundary_x ( )
-bcP = PeriodicBC ( W.sub ( 0 ), pbc_x )
+
+
+class PeriodicBoundary(SubDomain):
+    def inside(self, x, on boundary):
+        #return x[0] == 0 and on boundary
+
+    def map(self, x, y):
+        y[0] = x[0] - Le
+        y[1] = x[1]
+
+pbc_x = PeriodicBoundary_x()
+bcP = PeriodicBC(W.sub(0), pbc_x)
+
+
 #
 #  Define the variational problem: a(u,v) = L(v).
 #
-( v_i, q_i ) = TestFunctions ( W )
-( u_i, p_i ) = TrialFunctions ( W )
-a = ( 0.5 * mu * inner ( grad ( v_i ) + grad ( v_i ).T, grad ( u_i ) + grad ( u_i ).T ) \
-  - div ( v_i )* pi + q_i * div ( u_i ) ) * dx
-L = inner ( v_i, G ) * dx
+(v_i, q_i) = TestFunctions(W)
+(u_i, p_i) = TrialFunctions(W)
+a = (0.5 * mu * inner(grad(v_i) + grad(v_i).T, grad(u_i) + grad(u_i).T)
+     - div(v_i) * pi + q_i * div(u_i)) * dx
+L = inner(v_i, G) * dx
 #
 #  Matrix assembly.
 #
-U = Function ( W )
+U = Function(W)
 #
 #  Solution.
 #
-solve ( a == L, U, [ bcD, bcP ] )
+solve(a == L, U, [bcD, bcP])
 #
 #  Split the mixed solution to recover velocity and pressure.
 #
-( u, p ) = U.split ( )
+(u, p) = U.split()
