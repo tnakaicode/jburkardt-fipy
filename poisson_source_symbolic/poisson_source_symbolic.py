11647c037a0f52ef9200cafc584641b71fad4832
TakuNakai <tnakaicode@gmail.com>
2020-03-06 22:25:14 +0900

    poisson

--- poisson_source_symbolic/poisson_source_symbolic.py
+++ poisson_source_symbolic/poisson_source_symbolic.py
@@ -2,108 +2,111 @@
 #
 from fenics import *
 
-def poisson_source_symbolic ( ):
 
-#*****************************************************************************80
-#
-## poisson_source_symbolic generates the right hand side of a Poisson problem.
-#
-#  Discussion:
-#
-#    We are interested in solving an equation of the form:
-#
-#      - div kappa(u,x,y) grad u = f in Omega
-#                              u = g on dOmega
-#
-#    We have chosen:
-#      u(x,y) = 2 * ( 1 + y ) / ( (3+x)^2 +(1+y)^2 )
-#      kappa(u,x,y) = 1
-#      
-#    The corresponding f(x,y) is now determined.  But instead of computing it
-#    by hand, we ask the Python symbolic package to discover the formula:
-#      f(x,y) = 0
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license.
-#
-#  Modified:
-#
-#    30 October 2018
-#
-#  Author:
-#
-#    John Burkardt
-#
-  import matplotlib.pyplot as plt
-  import sympy as sym
+def poisson_source_symbolic():
 
-  def kappa ( u ):
-    value = 1
-    return value
+    #*****************************************************************************80
+    #
+    ## poisson_source_symbolic generates the right hand side of a Poisson problem.
+    #
+    #  Discussion:
+    #
+    #    We are interested in solving an equation of the form:
+    #
+    #      - div kappa(u,x,y) grad u = f in Omega
+    #                              u = g on dOmega
+    #
+    #    We have chosen:
+    #      u(x,y) = 2 * ( 1 + y ) / ( (3+x)^2 +(1+y)^2 )
+    #      kappa(u,x,y) = 1
+    #
+    #    The corresponding f(x,y) is now determined.  But instead of computing it
+    #    by hand, we ask the Python symbolic package to discover the formula:
+    #      f(x,y) = 0
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+    #  Modified:
+    #
+    #    30 October 2018
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+    import matplotlib.pyplot as plt
+    import sympy as sym
 
-  x, y = sym.symbols ( 'x[0], x[1]' )
-  u = 2.0 * ( 1.0 + y ) / ( ( 3.0 + x ) ** 2 + ( 1.0 + y ) ** 2 )
+    def kappa(u):
+        value = 1
+        return value
 
-  f = - sym.diff ( kappa ( u ) * sym.diff ( u, x ), x ) \
-      - sym.diff ( kappa ( u ) * sym.diff ( u, y ), y )
+    x, y = sym.symbols('x[0], x[1]')
+    u = 2.0 * (1.0 + y) / ((3.0 + x)**2 + (1.0 + y)**2)
 
-  f = sym.simplify ( f )
+    f = - sym.diff ( kappa ( u ) * sym.diff ( u, x ), x ) \
+        - sym.diff ( kappa ( u ) * sym.diff ( u, y ), y )
 
-  f_code = sym.printing.ccode ( f )
-  print ( '' )
-  print ( '  Formula for F(x,y) computed by SymPy:' )
-  print ( '' )
-  print ( '  f(x,y) = ', f_code )
-#
-#  Terminate.
-#
-  return
+    f = sym.simplify(f)
 
-def poisson_source_symbolic_test ( ):
+    f_code = sym.printing.ccode(f)
+    print('')
+    print('  Formula for F(x,y) computed by SymPy:')
+    print('')
+    print('  f(x,y) = ', f_code)
+    #
+    #  Terminate.
+    #
+    return
 
-#*****************************************************************************80
-#
-## poisson_source_symbolic_test tests poisson_source_symbolic.
-#
-#  Modified:
-#
-#    25 October 2018
-#
-#  Author:
-#
-#    John Burkardt
-#
-  import time
 
-  print ( time.ctime ( time.time() ) )
-#
-#  Report level = only warnings or higher.
-#
-  level = 30
-  set_log_level ( level )
+def poisson_source_symbolic_test():
 
-  print ( '' )
-  print ( 'poisson_source_symbolic_test:' )
-  print ( '  FENICS/Python version' )
-  print ( '  Generate the right hand side function f(x,y)' )
-  print ( '  for a Poisson problem' )
-  print ( '  - div kappa(u,x,y) grad u = f(x,y) in Omega' )
-  print ( '                          u = g(x,y) on dOmega' )
-  print ( '  where we have already chosen u(x,y) and kappa(x,y).' )
-  print ( '  and want f(x,y) computed symbolically by SymPy.' )
+    #*****************************************************************************80
+    #
+    ## poisson_source_symbolic_test tests poisson_source_symbolic.
+    #
+    #  Modified:
+    #
+    #    25 October 2018
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+    import time
 
-  poisson_source_symbolic ( )
-#
-#  Terminate.
-#
-  print ( '' )
-  print ( 'poisson_source_symbolic_test:' )
-  print ( '  Normal end of execution.' )
-  print ( '' )
-  print ( time.ctime ( time.time() ) )
-  return
+    print(time.ctime(time.time()))
+    #
+    #  Report level = only warnings or higher.
+    #
+    level = 30
+    set_log_level(level)
 
-if ( __name__ == '__main__' ):
+    print('')
+    print('poisson_source_symbolic_test:')
+    print('  FENICS/Python version')
+    print('  Generate the right hand side function f(x,y)')
+    print('  for a Poisson problem')
+    print('  - div kappa(u,x,y) grad u = f(x,y) in Omega')
+    print('                          u = g(x,y) on dOmega')
+    print('  where we have already chosen u(x,y) and kappa(x,y).')
+    print('  and want f(x,y) computed symbolically by SymPy.')
 
-  poisson_source_symbolic_test ( )
+    poisson_source_symbolic()
+    #
+    #  Terminate.
+    #
+    print('')
+    print('poisson_source_symbolic_test:')
+    print('  Normal end of execution.')
+    print('')
+    print(time.ctime(time.time()))
+    return
+
+
+if (__name__ == '__main__'):
+
+    poisson_source_symbolic_test()
